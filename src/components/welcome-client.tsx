2cb5def51d9fddd23be6d070246ee22038014593
Amit <akmaurya2657@gmail.com>
2025-06-27 14:58:59 +0530

    welcome page

--- src/components/welcome-client.tsx
+++ src/components/welcome-client.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 
 export function WelcomeClient() {
-    return (
-        <div className="flex flex-col sm:flex-row gap-4">
-            <Button
-              onClick={() => signIn("google", { callbackUrl: "/home" })}
-              size="lg"
-              className="font-bold text-lg"
-            >
-              Get Started with Google
-            </Button>
-        </div>
-    )
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-4">
+      <Button
+        onClick={() => router.push("/login")}
+        size="lg"
+        className="font-bold text-lg"
+      >
+        Get Started
+      </Button>
+    </div>
+  );
 }
